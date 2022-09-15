8e78254651fb1e32cc2a2e687f2001bbe2faeef2
Grisha <hersheleh@gmail.com>
2022-09-15 15:46:19 -0700

    perfect shuffle with python list and append

--- card_shuffle.py
+++ card_shuffle.py
@@ -13,26 +13,46 @@ class Card:
 class DeckOfCards:
     faces = ('Jack', 'Queen', 'King', 'Ace')
     suites = ('Clubs', 'Diamonds', 'Hearts', 'Spades')
-    
+
     def __init__(self):
         self.deck = []
+
         for suite in self.suites:
             for i in range(2, 11):
                 self.deck.append(Card(i, suite))
             for face in self.faces:
                 self.deck.append(Card(face, suite))
 
+
     def print_deck(self):
+        print("-------------------------")
         for card in self.deck:
             print (" {} ".format(card))
+        print("-------------------------")
 
     def perfect_shuffle(self):
+        split_point = len(self.deck)//2 # integer division
+        trailing_half = self.deck[split_point:]
+        del self.deck[split_point:]
+
+        j = 0
+        for i in range(0, len(trailing_half)):
+            self.deck.insert(j+1, trailing_half[i])
+            j = j + 2
+
+    def stack_shuffle(self):
         
 
+def test():
+    new_deck = DeckOfCards()
+    new_deck.print_deck()
+    #breakpoint()
+    for i in range(0, 20):
+        new_deck.perfect_shuffle()
+    new_deck.print_deck()
 
 
 
 if __name__ == '__main__':
-    new_deck = DeckOfCards()
-    new_deck.print_deck()
+    test()
     
