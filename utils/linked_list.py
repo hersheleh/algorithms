2257fd9c83c110e4a5d6111547d96b9c753b99f0
Grisha <hersheleh@gmail.com>
2022-09-15 14:05:20 -0700

    added utils directory. linked list work in progress

---
+++ utils/linked_list.py
@@ -0,0 +1,26 @@
+class ListNode:
+
+    def __init__(self, data=None, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+class LinkedList:
+
+    def __init__(self, None):
+        self.head = None
+        self.tail = None
+        self.size = 0
+        self.sentinel = self.head
+
+    def insert_next(self, element):
+        new_element = ListNode(element)
+
+        if (self.size == 0):
+            self.head = new_element
+            self.tail = new_element
+        else:
+            self.tail.next_node = new_element
+            self.tail = new_element
+        
+    
+    
