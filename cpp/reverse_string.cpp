98b650a655baaa776a8ea154b89d9b326b84defd
Grisha <hersheleh@gmail.com>
2022-09-26 19:53:18 -0700

    implemented reverse words using getline()

--- cpp/reverse_string.cpp
+++ cpp/reverse_string.cpp
@@ -1,9 +1,35 @@
+#include <cstdlib>
 #include <cstring>
+#include <sstream>
 #include <string>
 #include <iostream>
 #include <cstring>
 
 using namespace std;
+
+
+string reverseWords_getline(string s) {
+  string token;
+  istringstream iss(s);
+  string output;
+
+  while (getline(iss, token, ' ')) {
+
+    int word_start = 0;
+    int word_end = token.length() - 1;
+
+    while(word_start < word_end) {
+
+      char temp = token[word_start];
+      token[word_start] = token[word_end];
+      token[word_end] = temp;
+      word_start++;
+      word_end--;
+    }
+    output += token + " ";
+  }
+  return output.substr(0, s.length());
+}
 
 /*Takes a space serparated string as input and
   reverses each word */
@@ -43,6 +69,8 @@ string reverseWords(string s) {
 }
 
 int main(int argc, char * argv[]) {
+  // string out = reverseWords_getline("Let's take LeetCode contest");
+  // cout << out << endl;
 
   reverseWords("Let's take LeetCode contest");
 
