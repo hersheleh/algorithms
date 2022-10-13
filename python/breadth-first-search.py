5962bf4a10feff494903ff07bb349c2c6c5f0a46
Grisha <hersheleh@gmail.com>
2022-10-12 18:22:51 -0700

    stared BFS & DFS implementation

---
+++ python/breadth-first-search.py
@@ -0,0 +1,21 @@
+"""Breadth first seach implementation in python."""
+
+
+def BFS(G, root):
+    """Implement Breadth First Search algorithm."""
+    a = "what"
+    c = 0
+    for i in range(0, 10):
+        print("I am debugging")
+        c = i + c
+        print(i)
+    print(a)
+    print("Hello Grisha")
+
+
+def HelloGGG():
+    """Do it."""
+
+
+if __name__ == '__main__':
+    BFS(0, 0)
---
+++ python/depth-first-search.py
@@ -0,0 +1,1 @@
+"""Placeholder  Comment."""
