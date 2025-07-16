c2291bd03a3cb3fe13a64b7c3d3d43b8513f8afd
Grisha Khachaturyan <grisha@perfectcircuit.com>
2025-07-16 16:50:35 -0700

    started roman to integer problem: looks interesting

---
+++ cpp/roman-to-integer.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Solution {
+public:
+    int romanToInt(string s) {
+      map<string, int> m{{"I", 1}, {"V", 5}, {"X", 10}, {"L", 50}, {"C", 100}, {"D", 500}, {"M", 1000}};
+
+    }
+};
+
+int main() {
+  Solution *s = new Solution()
+}
